0bdf485ff2525cfb777ec3bcc0730d124ead89f1
Julia Albig <juliaalbig@outlook.com>
2013-11-18 10:33:00 -0600

    Added a category table, and FK column in Products table

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
 	has_many :line_items
 	has_many :orders, through :line_items
-  attr_accessible :description, :name, :price, :stock_quantity
+	belongs_to :category
+  attr_accessible :description, :name, :price, :stock_quantity, :image_file, :category_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131118162619) do
+ActiveRecord::Schema.define(:version => 20131118163038) do
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "customers", :force => true do |t|
     t.string   "first_name"
@@ -53,6 +59,7 @@ ActiveRecord::Schema.define(:version => 20131118162619) do
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
     t.string   "image_file"
+    t.integer  "category_id"
   end
 
   create_table "provinces", :force => true do |t|
